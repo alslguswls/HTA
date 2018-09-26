42d83d5342a3c3908ab247f630270d57eef85050
jin <jin@LAPTOP-KBNQ1L5M>
2018-09-26 15:35:22 +0900

    db Shadow

--- HTA-semi/src/db/DBConnection.java
+++ HTA-semi/src/db/DBConnection.java
@@ -12,8 +12,9 @@ public class DBConnection {
 		Connection con = null;
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
-			String url="jdbc:oracle:thin:@dmszone.com:40021:xe";
-			con=DriverManager.getConnection(url, "semi","semi1004");
+			//필요하신분 연락 주세요~
+			String url="jdbc:oracle:thin:@localhost:1521:xe";
+			con=DriverManager.getConnection(url, "scott","tiger");
 		} catch (ClassNotFoundException ce) {
 			System.out.println(ce.getMessage());
 		} catch (SQLException se) {
